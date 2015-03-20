d8bb0cfcfc0a37a7fed99337cb63298c09bf6914
doruison <doruison@hotmail.com>
2015-03-20 13:16:14 +0800

    3.42

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include<cctype>
+#include<numeric>
+#include<list>
+#include <map>
+#include <cstring>
+
+
+
+
+using namespace std;
+
+//本版本：《primer》3.42
+
+
+int main(){
+    int num1[5]; int temp;
+    vector<int> num2;
+    cout<<"flags";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> temp;
+        num2.push_back(temp);
+
+
+
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        num1[i] = num2[i];
+
+
+
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << num1[i]<<" ";
+
+
+
+    }
+cout<<"000";
+
+
+
+}
