476a9aad7d65773d5f1e579837a5861970559aaa
doruison <doruison@hotmail.com>
2015-04-03 13:47:49 +0800

    运算符重载 7.40；;14.5；;14.8；;14.12；14.15;14.17；;4.19

--- main.cpp
+++ main.cpp
@@ -16,56 +16,93 @@ using namespace std;
 
 
 
-class HasPtr{
-public:
-HasPtr(const std::string & s=std::string()) :
-ps(new std::string(s)),i(0){}
-~HasPtr(){
-    delete ps;
+class Book{
+    friend std::istream &operator >>(std::istream& is,Book &b);
+ friend   bool operator!=(const Book &a,const Book &b);
+ friend   bool operator==(const Book &a,const Book &b);
+
+    friend std::ostream &operator<<(std::ostream & os,const Book & b);
+public :Book(int p,string i,string n):price(p),ISBN(i),name(n){}
+    Book():price(0),ISBN(" "),name(" "){}
+    Book &operator+=(const Book &b){
+        price+=b.price;
+        ISBN+=b.ISBN;
+        name+=b.name;
+        return *this;
 
 
-}
-HasPtr(const HasPtr& );
-string const getps (){return *ps;}
-HasPtr &operator=(const HasPtr& in){
 
-    *ps=*(in.ps);
-    i=in.i;
-    return *this;
-
-}
+    }
 private :
-std::string *ps;
-int i;
-
+    int price;
+    string ISBN;
+    string name;
 
 
 
 };
+bool operator!=(const Book &a,const Book &b)
+{
+    return !(a==b);
 
 
+}
 
-HasPtr::HasPtr(const HasPtr &in):
-    ps(new std::string(" ")),i(0){
+bool operator==(const Book &a,const Book &b)
+{
+    if(a.price==b.price&&a.name==b.name&&a.ISBN==b.ISBN)
+    return true;
+    else return false;
 
-*ps=*(in.ps);
-i=in.i;
+
+}
+
+Book operator+(Book &a ,Book & b)
+{
+    Book c=a;
+    c+=b;
+
+    return c;
+}
+
+std::istream &operator >>(std::istream& is,Book &b)
+{
+
+    is>>b.price>>b.ISBN>>b.name;
+    if(!is)
+        b=Book();
+    return is;
+
 
 }
 
 
+
+std::ostream &operator<<(std::ostream & os,const Book & b)
+{
+
+
+
+
+
+    os<<"price:"+to_string(b.price);
+    os<<";ISBN:"+b.ISBN;
+    os<<";name:"+b.name;
+    return os;
+}
+
+
+
+
+
+
+
 int main(){
+    Book a(10,"123456","上册");
+    Book b(10,"123456","上册");
 
-HasPtr hp2("hp2");
-cout<<hp2.getps();
-string s;
-cin>>s;
-auto hp1(s);
-hp2=hp1;
-cout<<hp2.getps();
 
-cout<<"flag";
-
+    cout<<(a==b);
 
 
 
