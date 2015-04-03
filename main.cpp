3637b1e37c2de4f4842ffdc0369ef068d584ba3f
doruison <doruison@hotmail.com>
2015-04-03 15:11:30 +0800

    14.35

--- main.cpp
+++ main.cpp
@@ -16,96 +16,33 @@ using namespace std;
 
 
 
-class Book{
-    friend std::istream &operator >>(std::istream& is,Book &b);
- friend   bool operator!=(const Book &a,const Book &b);
- friend   bool operator==(const Book &a,const Book &b);
-
-    friend std::ostream &operator<<(std::ostream & os,const Book & b);
-public :Book(int p,string i,string n):price(p),ISBN(i),name(n){}
-    Book():price(0),ISBN(" "),name(" "){}
-    Book &operator+=(const Book &b){
-        price+=b.price;
-        ISBN+=b.ISBN;
-        name+=b.name;
-        return *this;
+class input{
+public:
+    input(istream &i=cin,char c=' '):is(i),sep(c){}
 
 
+    void operator()( string &s)
+    {
+
+        is>>s;
+s+=sep;
 
     }
+
+
+
 private :
-    int price;
-    string ISBN;
-    string name;
-
-
+    istream &is;
+    char sep;
 
 };
-bool operator!=(const Book &a,const Book &b)
-{
-    return !(a==b);
-
-
-}
-
-bool operator==(const Book &a,const Book &b)
-{
-    if(a.price==b.price&&a.name==b.name&&a.ISBN==b.ISBN)
-    return true;
-    else return false;
-
-
-}
-
-Book operator+(Book &a ,Book & b)
-{
-    Book c=a;
-    c+=b;
-
-    return c;
-}
-
-std::istream &operator >>(std::istream& is,Book &b)
-{
-
-    is>>b.price>>b.ISBN>>b.name;
-    if(!is)
-        b=Book();
-    return is;
-
-
-}
-
-
-
-std::ostream &operator<<(std::ostream & os,const Book & b)
-{
-
-
-
-
-
-    os<<"price:"+to_string(b.price);
-    os<<";ISBN:"+b.ISBN;
-    os<<";name:"+b.name;
-    return os;
-}
-
-
-
-
 
 
 
 int main(){
-    Book a(10,"123456","上册");
-    Book b(10,"123456","上册");
-
-
-    cout<<(a==b);
-
-
-
-
+    input i(cin,'\t');
+string s="zsf";
+i(s);
+cout<<s;
 
 }
