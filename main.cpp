df9a67a4287517fa5b9ce3fa48c8087ca19a1661
doruison <doruison@hotmail.com>
2015-04-04 18:16:16 +0800

    14.36 37
    1.函数对象可以用作泛型算法请求的函数参数，不必带括号加参数；带括号加的是构造函数参数，也能先定义再给出：c
    2.泛型目的地dest为插入迭代器包装后的容器，而不能是裸迭代器
    3.字符串比较包含空白符，打印时不一定能看出来。这是一个问题。
    4.VS的调试其实很方便
    
    5.Clang即使在O0下仍会严重打乱原语句顺序。因此简单的cout二分法定位就行不通了。比如本例，修复前clang生成程序到exit输入完错误，被误导input处理输入exit有误或离开有误，折腾半天；VS则打出vs向量才崩，证明是remove_copy_if的使用不当

--- main.cpp
+++ main.cpp
@@ -21,12 +21,15 @@ public:
     input(istream &i=cin,char c=' '):is(i),sep(c){}
 
 
-    void operator()( string &s)
+   bool operator()( string &s)
     {
-
-        is>>s;
-s+=sep;
-
+		is >> s;
+		if (s!="exit")
+		{
+			s += sep;
+			return true;
+		}
+		else return false;
     }
 
 
@@ -38,11 +41,51 @@ private :
 };
 
 
+class check{
+public:
+	check(string init="0") :del(init){}
+
+
+	bool operator()(string &s)
+	{
+		cout << "  s:" + s + " init:" + del << endl;
+		if (del+"\t" == s)
+		{
+			cout << "Hit!";
+			return true;
+		}
+		else {
+			cout << "!Hit";
+			return false;
+
+		}
+	}
+
+
+private:
+	string del;
+
+};
+
 
 int main(){
     input i(cin,'\t');
-string s="zsf";
-i(s);
-cout<<s;
+	string s;
+	check c("del");
+	vector<string> vs, vs2;
+	while (i(s))
+		vs.push_back(s);
+
+	for (auto i = vs.begin(); i != vs.end(); i++)
+		cout << *i;
+	cout << endl;
+	cout << "flags";
+	string d = "k";
+	remove_copy_if(vs.begin(), vs.end(),back_inserter(vs2),c);
+
+
+	for (auto i = vs2.begin(); i != vs2.end(); i++)
+		cout << *i;
+
 
 }
