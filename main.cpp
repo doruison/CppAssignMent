7c4e75e0c44d3af6dd064680285de4103cc8c1d0
doruison <doruison@hotmail.com>
2015-04-05 21:36:06 +0800

    增加Primer示例

--- main.cpp
+++ main.cpp
@@ -14,78 +14,18 @@
 
 using namespace std;
 
-
-
-class input{
+class Quote{
 public:
-    input(istream &i=cin,char c=' '):is(i),sep(c){}
+	std::string isbn() const;
+	virtual double net_price(std::size_t n)const;
+};
 
-
-   bool operator()( string &s)
-    {
-		is >> s;
-		if (s!="exit")
-		{
-			s += sep;
-			return true;
-		}
-		else return false;
-    }
+class Bulk_Quote :public Quote{
+public :
+	double net_price(std::size_t) const override;
 
 
 
-private :
-    istream &is;
-    char sep;
 
 };
 
-
-class check{
-public:
-	check(string init="0") :del(init){}
-
-
-	bool operator()(string &s)
-	{
-		cout << "  s:" + s + " init:" + del << endl;
-		if (del+"\t" == s)
-		{
-			cout << "Hit!";
-			return true;
-		}
-		else {
-			cout << "!Hit";
-			return false;
-
-		}
-	}
-
-
-private:
-	string del;
-
-};
-
-
-int main(){
-    input i(cin,'\t');
-	string s;
-	check c("del");
-	vector<string> vs, vs2;
-	while (i(s))
-		vs.push_back(s);
-
-	for (auto i = vs.begin(); i != vs.end(); i++)
-		cout << *i;
-	cout << endl;
-	cout << "flags";
-	string d = "k";
-	remove_copy_if(vs.begin(), vs.end(),back_inserter(vs2),c);
-
-
-	for (auto i = vs2.begin(); i != vs2.end(); i++)
-		cout << *i;
-
-
-}
