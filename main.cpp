6d2d49b063b8abc0d75d74a61fa3a9fd4cc90fd9
doruison <doruison@hotmail.com>
2015-04-06 10:16:25 +0800

    15.2 3 5 6

--- main.cpp
+++ main.cpp
@@ -16,16 +16,68 @@ using namespace std;
 
 class Quote{
 public:
-	std::string isbn() const;
-	virtual double net_price(std::size_t n)const;
+	Quote() = default;
+	Quote(const string &book, double sales_price) :bookNo(book), price(sales_price){}
+	string isbn() const { return bookNo; }
+	virtual double net_price(size_t n) const
+	{
+		return n*price;
+}
+	virtual ~Quote(){};
+private :string bookNo;
+protected: double price = 0;
+
+
+
 };
 
 class Bulk_Quote :public Quote{
 public :
-	double net_price(std::size_t) const override;
+	Bulk_Quote(const string & book, double p, size_t qty, double disc) :
+		Quote(book, p), min_qty(qty), discount(disc){};
 
+	double net_price(size_t cnt)const
+	{
+		if (cnt >= min_qty)
+			return cnt*(1 - discount)*price;
+		else return cnt*price;
+	
+	}
+private:
+	size_t min_qty = 0;
+	double discount = 0;
 
 
 
 };
 
+double print_total(ostream &os, const Quote &item, size_t n)
+{
+	double ret = item.net_price(n);
+	os << "isbn:" << item.isbn() << "#sold:"
+		<< n << "total due :" << ret << endl;
+	return ret;
+
+
+
+
+}
+
+int main(){
+	Quote q("9787121",35);
+	Bulk_Quote bq("9787121", 35, 100, 0.12);
+	Quote* qp;
+	qp = &q;
+	print_total(cout,*qp,150);
+	qp = &bq;
+	print_total(cout, *qp, 150);
+	getchar();
+
+
+
+
+
+
+
+
+}
