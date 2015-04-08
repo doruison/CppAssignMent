bfde03398c30c852e932225022ae152511e40060
doruison <doruison@hotmail.com>
2015-04-08 11:02:30 +0800

    本版本：《primer》12.7并修正12.6

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include<cctype>
 #include<numeric>
-#include <fstream>
-#include "headtest.h" 
+#include <memory>
 
 #include <cstring>
 
@@ -15,26 +13,42 @@
 
 using namespace std;
 
+//本版本：《primer》12.7并修正12.6
+
+
+shared_ptr< vector<int>> gen()
+{
+	return make_shared < vector<int> >();
+
+
+}
 
 
 
+void deal(shared_ptr< vector<int>> p)
+{
+	int in;
+	while (cin >> in)
+		p->push_back(in);
+
+
+}
+
+void print(shared_ptr< vector<int>> p)
+{
+	int it = 0;
+	for (auto i = p->begin(); i != p->end(); i++)
+	{
+		it = *i;
+		cout << it;
+	}
+}
 
 int main(){
 
-	HasPtr hp2("hp2");
-	cout << hp2.getps();
-	string s;
-	cin >> s;
-	auto hp1(s);
-	hp2 = hp1;
-	hp2 = hp2;
-	cout << hp2.getps();
-
-	cout << "flag";
-
-	getchar();
-	getchar();
-
+	shared_ptr< vector<int>> p = gen();
+	deal(p);
+	print(p);
 
 
 
